af08831ae8572becd691391aadff2bfeef094c0e
hwj <1965119558@qq.com>
2022-04-19 17:17:38 +0800

    打包压缩，打包设置base路径防止打包后访问404

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,33 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
+import viteCompression from 'vite-plugin-compression'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/vue3System/',//基础路径 对应nginx的location和打包出来href="/vue3System/static/*“
   resolve: {
     alias: {
       '@': path.resolve(__dirname, "src")
     },
   },
-  plugins: [vue()],
+  plugins: [vue(), viteCompression({
+    verbose: true,
+    disable: false,
+    threshold: 10240,
+    algorithm: 'gzip',
+    ext: '.gz',
+  })],
+  build: {
+    rollupOptions: {
+      output: {
+        chunkFileNames: 'static/js/[name]-[hash].js',
+        entryFileNames: 'static/js/[name]-[hash].js',
+        assetFileNames: 'static/[ext]/[name]-[hash].[ext]',
+      }
+    }
+  },
   server: {
     host: '127.0.0.1',
     port: 8802,
